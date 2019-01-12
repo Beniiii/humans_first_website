81bf3635c812d9e342fb9e7e94f35947d5b13678
Benjamin @ Huber <benjamin.huber@students.fhnw.ch>
2019-01-12 15:53:31 +0100

    bla

---
+++ resources/js/javascript.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  // MODAL
+  $('.sidenav').sidenav();
+  $('.fixed-action-btn').floatingActionButton();
+  $('.tooltipped').tooltip();
+});
+
+var header = $('header');
+var range = 200;
+
+$(window).on('scroll', function() {
+  var scrollTop = $(this).scrollTop(),
+    height = header.outerHeight(),
+    offset = height / 1.1,
+    calc = 1 - (scrollTop - offset + range) / range;
+
+  header.css({ opacity: calc });
+
+  if (calc > '1') {
+    header.css({ opacity: 1 });
+  } else if (calc < '0') {
+    header.css({ opacity: 0 });
+  }
+});
